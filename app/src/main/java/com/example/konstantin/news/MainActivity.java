414b3e8994709213c87b0a57298e1e5f8874ea19
Konstantin <mr.ks83@mail.ru>
2017-10-08 00:22:13 +0300

    add slider

--- app/src/main/java/com/example/konstantin/news/MainActivity.java
+++ app/src/main/java/com/example/konstantin/news/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.konstantin.news;
 
 import android.os.AsyncTask;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -21,11 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView StoriesRecyclerView;
     private List<GalleryItem> mItems = new ArrayList<>();
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager=(ViewPager)findViewById(R.id.viewPager);
+        ViewPagerAdapter viewPagerAdapter=new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
 
         TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
         tabHost.setup();
